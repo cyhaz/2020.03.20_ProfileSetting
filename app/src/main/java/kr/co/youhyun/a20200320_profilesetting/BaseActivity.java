acf1bb9669f4c9835625e71a448eb095b610d12c
cyhaz <cyhaz@naver.com>
2020-03-20 09:53:02 +0900

    초기화면 구성  - 라이브러리로 이미지 동그랗게

---
+++ app/src/main/java/kr/co/youhyun/a20200320_profilesetting/BaseActivity.java
@@ -0,0 +1,13 @@
+package kr.co.youhyun.a20200320_profilesetting;
+
+import android.content.Context;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+    public Context mContext = this;
+
+    public abstract void setupEvents();
+    public abstract void setValues();
+}
---
+++ app/src/main/java/kr/co/youhyun/a20200320_profilesetting/MainActivity.java
@@ -0,0 +1,24 @@
+package kr.co.youhyun.a20200320_profilesetting;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void setupEvents() {
+
+    }
+
+    @Override
+    public void setValues() {
+
+    }
+}
